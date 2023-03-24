405de61dda23ceacc5fc954b932a0e8b233dbdc4
GardenJB <122426140+GardenJB@users.noreply.github.com>
2023-03-24 23:52:15 +0900

    Update 최댓값 만들기 （1）.java

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -5,6 +5,7 @@ class Solution {
         int max2 =0;
         //가장 큰 두 수 갱신하기
         for(int x : numbers){
+            //순서에 주의**** 두값이 모두 채워져야 함
             if(x>=max2){
                 max2=max1;
                 max1=x;
