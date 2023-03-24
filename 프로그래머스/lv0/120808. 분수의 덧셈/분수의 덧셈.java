c26deeb11fa281568f6ca1b7a613ffe1c97e3e41
GardenJB <122426140+GardenJB@users.noreply.github.com>
2023-03-24 23:06:25 +0900

    Update 분수의 덧셈.java

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -2,8 +2,11 @@ class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = new int[2];
         
+        //분자
         int top = numer1*denom2+numer2*denom1;
+        //분모
         int bot = denom1*denom2;
+        //기약분수 > 최대공약수 있다면 나누기 > 분자 분모 둘 중 작은값 기준
         for(int i=Math.min(top, bot); i>=1; i--) {
             if(top%i==0 && bot%i==0){
                 answer[0]=top/i;
