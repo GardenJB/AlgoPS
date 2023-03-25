ed1e8522c05476437d75fd48c9ac953e3c043298
GardenJB <122426140+GardenJB@users.noreply.github.com>
2023-03-25 17:14:54 +0900

    [level 0] Title: 모음 제거, Time: 0.16 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -2,6 +2,7 @@ class Solution {
     public String solution(String my_string) {
         String answer="";
         answer = my_string.replaceAll("[aeiou]","");
+        //answer = my_string.replaceAll("a||e||i||o||u", "");
         return answer;
     }
 }
