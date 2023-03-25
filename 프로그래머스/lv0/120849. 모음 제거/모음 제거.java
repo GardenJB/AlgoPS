77ac2fbf9dfd023e91dde1e10eb05f1fa8c63cb9
GardenJB <122426140+GardenJB@users.noreply.github.com>
2023-03-25 17:19:47 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,6 +1,12 @@
 class Solution {
     public String solution(String my_string) {
         String answer="";
+        
+        /* String과 char 구별
+        * 조건 > String에 같은 원소가 여러번?
+        * || vs | 전자는 조건 만족 경우 바로 중지
+        */
+        
         //answer = my_string.replaceAll("[aeiou]","");
         //answer = my_string.replaceAll("a|e|i|o|u", "");
         String[] vow = {"a", "e", "i", "o", "u"};
