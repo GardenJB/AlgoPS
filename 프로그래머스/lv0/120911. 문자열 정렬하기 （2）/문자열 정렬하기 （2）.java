e1b73a6a7c3a3e8aef7068fc1ed78ca02e41a4da
GardenJB <122426140+GardenJB@users.noreply.github.com>
2023-03-25 22:54:59 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 2.02 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
+        String answer="";
         String[] arr = my_string.toLowerCase().split("");
         
         Arrays.sort(arr);
@@ -9,6 +9,9 @@ class Solution {
         for(int i=0; i<arr.length; i++){
             answer += arr[i];
         }
+        
+        //arr이 char인 경우 : 가
+        //answer = new String(arr);
         return answer;
     }
 }
