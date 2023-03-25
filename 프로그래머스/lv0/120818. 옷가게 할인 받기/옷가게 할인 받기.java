bd84c10d3262cae4048d3aec15eabdc2e7d6aac8
GardenJB <122426140+GardenJB@users.noreply.github.com>
2023-03-25 16:44:11 +0900

    Update 옷가게 할인 받기.java

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -2,6 +2,7 @@ class Solution {
     public int solution(int price) {
         int answer = 0;
         if(price >=500000)
+            //answer = (int) (price*0.8);
             answer = price * 8/10;
         else if(price >= 300000)
             answer = price * 9/10;
