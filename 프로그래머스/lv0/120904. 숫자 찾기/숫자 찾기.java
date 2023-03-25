a3eaa7cd749dd9ab0507bbc436f5caa156a5b613
GardenJB <122426140+GardenJB@users.noreply.github.com>
2023-03-25 22:49:44 +0900

    [level 0] Title: 숫자 찾기, Time: 0.19 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,16 +1,21 @@
 class Solution {
     public int solution(int num, int k) {
         int answer = 0;
-        String temp = num+"";
-        boolean ch = false;
-        for(int i=0; i<temp.length(); i++) {
-            if(Integer.parseInt(temp.valueOf(temp.charAt(i)))==k) {
-                answer = i+1;
-                ch=true;
-                break;
-            }
-            if(!ch) answer = -1;
-        }
+        // String temp = num+"";
+        // boolean ch = false;
+        // for(int i=0; i<temp.length(); i++) {
+        //     if(Integer.parseInt(temp.valueOf(temp.charAt(i)))==k) {
+        //         answer = i+1;
+        //         ch=true;
+        //         break;
+        //     }
+        //     if(!ch) answer = -1;
+        // }
+        
+        String n = String.valueOf(num);
+        
+        System.out.println(n);
+        answer = n.indexOf(String.valueOf(k))>=0? n.indexOf(String.valueOf(k))+1 : -1;
         return answer;
     }
 }
