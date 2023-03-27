89523b86345968f47925613872b61bad2ea4f9cd
GardenJB <122426140+GardenJB@users.noreply.github.com>
2023-03-27 22:22:04 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 200 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String st = sc.next();
+		
+		st=st.replaceAll("c=|c-|dz=|d-|lj|nj|s=|z=", "1");
+		
+		int ans = st.length();
+		System.out.println(ans);
+	}
+
+}
