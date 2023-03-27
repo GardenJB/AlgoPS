14dfde2c7208f6a2f5081b9445e99c1c16d97a46
GardenJB <122426140+GardenJB@users.noreply.github.com>
2023-03-27 23:10:31 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -12,6 +12,9 @@ class Solution {
 		}else{
 			answer = -1;
 		}
+        
+        //시간 초과 *** 형변환, 다른 변수 활용 w??? 
+        
         // long answer = 0;
         // int i=1;
         // while(i*i<n){
@@ -21,6 +24,9 @@ class Solution {
         //     answer=(i+1)*(i+1);
         // else
         //     answer=-1;
+        
+        //시간 초과
+        
         // for(int i=1; i<=n; i++) {
         //     if(i*i==n){
         //         answer=(i+1)*(i+1);
@@ -28,6 +34,7 @@ class Solution {
         //     }
         // }
         // if(answer==0) answer=-1;
+        
         return answer;
     }
 }
