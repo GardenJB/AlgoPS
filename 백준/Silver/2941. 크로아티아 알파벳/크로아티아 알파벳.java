c76a087ef3496d8ebb218dd984d146d222e9a813
GardenJB <122426140+GardenJB@users.noreply.github.com>
2023-03-27 22:33:07 +0900

    Update 크로아티아 알파벳.java

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -7,8 +7,9 @@ public class Main {
 		String st = sc.next();
 		
 		//string.replaceAll("","")용법 주의*****
+		//해당 문자열을 한자리수 문자열로 바꾸기
 		st=st.replaceAll("c=|c-|dz=|d-|lj|nj|s=|z=", "1");
-		
+		//문자열 길이 정답
 		int ans = st.length();
 		System.out.println(ans);
 	}
