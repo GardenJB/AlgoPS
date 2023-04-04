c8a5daa0aa561c68c3e7a10f4e79359c09f2db2a
GardenJB <122426140+GardenJB@users.noreply.github.com>
2023-04-04 23:12:19 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.18 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,17 +1,19 @@
 class Solution {
     public int solution(String my_string) {
         int answer =0;
-		//my_string.replaceAll("[a-zA-Z]", ",");
-		//String[]temp = my_string.split(",");
-//		for(String s : temp) {
-//			System.out.println(s);
-//			//answer+=Integer.parseInt(s);
-//		}
-		String[]temp = my_string.split("[a-zA-Z]");
+        //***replaceAll
+		my_string = my_string.replaceAll("[a-zA-Z]", ",");
+		String[]temp = my_string.split(",");
+        //***split 결과
 		for(String s : temp) {
-			System.out.println(s);
-			answer += s.isEmpty()? 0 :Integer.parseInt(s);
+			//System.out.println(s);
+			if(!s.equals("")) answer+=Integer.parseInt(s);
 		}
+		// String[]temp = my_string.split("[a-zA-Z]");
+		// for(String s : temp) {
+		// 	//System.out.println(s);
+		// 	answer += s.isEmpty()? 0 :Integer.parseInt(s);
+		// }
         return answer;
     }
 }
