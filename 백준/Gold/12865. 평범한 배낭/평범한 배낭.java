3c06070d18f29597e8c966191cd977f26ea32342
GardenJB <122426140+GardenJB@users.noreply.github.com>
2023-04-06 00:29:57 +0900

    [Gold V] Title: 평범한 배낭, Time: 204 ms, Memory: 18556 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,44 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		//물품의 수  n
+		int n = Integer.parseInt(st.nextToken());
+		//최대 무게 k
+		int k = Integer.parseInt(st.nextToken());
+		
+		//물건 배열 무게 , 가치
+		int[][] bag = new int[n][2];
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			bag[i][0] = Integer.parseInt(st.nextToken());
+			bag[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		//Arrays.sort(bag, (o1, o2) -> o1[1] - o2[1]);
+		//dp
+		int[] dp = new int[k+1];
+		
+		for(int i=0; i<n; i++) {
+			for(int j=k; j>=0; j--) {
+				//무게 초과
+				if(bag[i][0]+j>k) continue;
+				//무게 가능 경우 비교
+				dp[bag[i][0]+j]= dp[j]+bag[i][1]> dp[bag[i][0]+j] ? dp[j]+bag[i][1] : dp[bag[i][0]+j];
+			}
+		}
+		//최대 가치
+		int max_v = Integer.MIN_VALUE;
+		for(int d : dp) {
+			if(max_v<d) max_v=d;
+		}
+		System.out.println(max_v);
+	}
+
+}
