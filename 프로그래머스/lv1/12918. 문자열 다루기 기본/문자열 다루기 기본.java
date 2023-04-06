aaa45ee30f9ad9a5a18a671423a01531ec2638b3
GardenJB <122426140+GardenJB@users.noreply.github.com>
2023-04-07 04:54:09 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if(!(s.length()==4 || s.length()==6) ) answer = false;
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i)!='0' & s.charAt(i)!='1' & s.charAt(i)!='2' & s.charAt(i)!='3' & s.charAt(i)!='4' & s.charAt(i)!='5' & s.charAt(i)!='6' & s.charAt(i)!='7' & s.charAt(i)!='8' & s.charAt(i)!='9' ) answer=false;
+        }
+        return answer;
+    }
+}
