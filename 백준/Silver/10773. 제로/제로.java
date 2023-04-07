c540aa8259d7fa212d29196555f1f14450eed4a8
GardenJB <122426140+GardenJB@users.noreply.github.com>
2023-04-07 22:03:07 +0900

    [Silver IV] Title: 제로, Time: 912 ms, Memory: 109732 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,32 @@
+
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//정수 k
+		int k = sc.nextInt();
+		
+		//수 배열
+		int[] arr = new int[k];
+		for(int i=0; i<k; i++) {
+			arr[i]= sc.nextInt();
+		}
+		
+		Stack<Integer> st = new Stack<>();
+		for(int a : arr) {
+			if(a==0) st.pop();
+			else st.add(a);
+		}
+		
+		int sum=0;
+		while(!st.isEmpty()) {
+			sum+=st.pop();
+		}
+		
+		System.out.println(sum);
+	}
+
+}
