ce121fdeb9205dd57759ce3f789586822434194b
GardenJB <122426140+GardenJB@users.noreply.github.com>
2023-04-21 02:02:06 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String s = sc.next();
+        
+        //형변환 자동
+        int a = s.charAt(0);
+        System.out.println(a);
+    }
+}
