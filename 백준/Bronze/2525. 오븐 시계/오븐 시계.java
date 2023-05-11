80385c77ec42f43401061a5ccc2db915dd40df45
GardenJB <122426140+GardenJB@users.noreply.github.com>
2023-05-11 22:29:42 +0900

    [Bronze III] Title: 오븐 시계, Time: 212 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int d = b+c;
+		if(d>59) {
+			//** 60의 배수
+			d=(b+c)%60;
+			a+=(b+c)/60;
+            //**
+			if(a>23) a=a-24;
+		}
+		
+		System.out.printf("%d %d", a, d);
+	}
+
+}
