7deea588bc2279895be1e5deff4e7067ad4340bc
GardenJB <122426140+GardenJB@users.noreply.github.com>
2023-09-13 19:28:37 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 216 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
@@ -0,0 +1,43 @@
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//총 사탕 수 n
+		int n = sc.nextInt();
+		
+		int ans = 0;
+		
+		/* 사탕 규칙
+		 * * 사탕 총 합
+		 * a + b + c = n
+		 * * 남규는 영훈보다 2개 이상 많
+		 * a > = b+2
+		 * * 택희가 받는 사탕의 수는 짝수
+		 * c % 2 == 0 
+		 * * 사탕 최대 수
+		 * n>= 1 && n <= 100
+		 * * 출력
+		 * 만족 경우의 수 | 0
+		 */
+
+		if(n > 3) {
+			for(int i=1; i<n; i++) {
+				for(int j=1; j<n; j++) {
+					for(int k=1; k<n; k++) {
+						if( i+j+k == n) {
+							if(i >= j+2) {
+								if(k%2 == 0) ans++;
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		System.out.println(ans);
+		
+	}
+}
