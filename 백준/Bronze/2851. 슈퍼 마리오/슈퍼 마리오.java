7e484ede3cf7485f3f873525c18696b809ac8fa2
GardenJB <122426140+GardenJB@users.noreply.github.com>
2024-01-01 19:16:56 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 200 ms, Memory: 17616 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -9,12 +9,9 @@ public class Main {
 		for(int i=0; i<10; i++) {
 			int temp = sc.nextInt();
 			sum+=temp;
-			//합이 100 초과 시점 멈추고 비교
 			if(sum>100) {
 				max_s = sum-100<= 100-(sum-temp) ? sum : sum-temp;
 				break;
-				
-			//총 합이 100이하인 경우***
 			}else if(i==9)
 				max_s = sum;
 		}
