414750976ec02e7c2539b480c60e6942b85a13e6
GardenJB <122426140+GardenJB@users.noreply.github.com>
2024-01-08 22:18:19 +0900

    [Gold V] Title: 실질적 약수, Time: 784 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Gold/2247. 실질적 약수/실질적 약수.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int n;
+
+    static long sum;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        sum = 0;
+        for (int i = 2; i < n-1; i++) {
+            sum += ((n/i)-1) * i;
+        }
+
+        System.out.println(sum%1000000);
+    
+    
+    }
+}
